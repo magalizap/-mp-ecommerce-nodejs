150a8e233d6cc1b8e2285b4ae653d1164529a43a
Magali Sol <magalisol2307@gmail.com>
2023-11-13 22:18:14 -0300

    nuevo commit

--- app.js
+++ app.js
@@ -1,22 +1,139 @@
-var express = require('express');
-var exphbs  = require('express-handlebars');
-var port = process.env.PORT || 3000
+import express from 'express'
+import { engine } from 'express-handlebars'
+import { __dirname } from "./utils/path.js"
+import * as path from 'path'
+import { MercadoPagoConfig, Preference } from 'mercadopago'
 
-var app = express();
- 
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
 
-app.use(express.static('assets'));
- 
-app.use('/assets', express.static(__dirname + '/assets'));
+const app = express()
+const port = process.env.PORT || 3000
 
-app.get('/', function (req, res) {
-    res.render('home');
-});
 
-app.get('/detail', function (req, res) {
-    res.render('detail', req.query);
-});
+const client = new MercadoPagoConfig({
+    accessToken: 'APP_USR-8709825494258279-092911-227a84b3ec8d8b30fff364888abeb67a-1160706432',
+    options: { timeout: 5000 },
+})
 
-app.listen(port);
+const preference = new Preference(client)
+
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+
+app.use(express.static('assets'))
+app.set('views', path.resolve(__dirname, './views')) 
+app.use('/assets', express.static(__dirname + '/assets'))
+
+app.use(express.json())  
+app.use(express.urlencoded({extended: true}))
+
+app.get('/', (req, res) => {
+    res.render('home', {view:'home'})
+})
+
+app.get('/detail', (req, res) => {
+    res.render('detail', {data: req.query, view:'item', script:'index.js'})
+})
+
+app.post('/detail', async (req, res) => {
+    const { title, unit, img, price, id } = req.body
+    
+
+    await preference.create( { body: {
+        items: [{
+            id: JSON.parse(id),
+            title: title,
+            description: 'Dispositivo móvil de Tienda e-commerce',
+            quantity: JSON.parse(unit),
+            unit_price: JSON.parse(price) ,
+            picture_url: img,
+        }],
+        payer:{
+            name: 'Lalo',
+            surname: 'Landa',
+            email: 'test_user_36961754@testuser.com',
+            phone: {
+                area_code: 'ARS',
+                number: 1124290671
+            },
+            address: {
+                street_name: 'calle falsa',
+                street_number: 123,
+                zip_code: 'B1708KDA' 
+            },
+            
+        },
+        payment_methods: {
+            excluded_payment_methods: [
+                {
+                    id: 'visa'
+                }
+            ],
+            installments: 6,
+        },
+        external_reference: 'magalisol2307@gmail.com',
+        back_urls: {
+            success: 'https://magalizap-mp-commerce-nodejs.onrender.com/success', 
+            failure: 'https://magalizap-mp-commerce-nodejs.onrender.com/failure',
+            pending: 'https://magalizap-mp-commerce-nodejs.onrender.com/pending'
+        },
+
+        notification_url: 'https://magalizap-mp-commerce-nodejs.onrender.com/webhook',
+        auto_return: 'approved',
+
+    }, requestOptions: {
+        integratorId: 'dev_24c65fb163bf11ea96500242ac130004',
+    }})
+    .then((response) => {
+        res.json({
+            id: response.id
+        })
+    })
+	.catch((error) => console.log(error))
+    //res.render('detail')
+})
+
+app.get('/success', (req, res) => {
+	res.render('success',{
+		payment: req.query.payment_id,
+		status: req.query.status,
+		merchantOrder: req.query.merchant_order_id,
+        externalReference: req.query.external_reference,
+        paymentType: req.query.payment_type
+	})
+})
+
+app.get('/failure', (req, res) => {
+	res.render('failure',{
+		payment: req.query.payment_id,
+		status: req.query.status,
+		merchantOrder: req.query.merchant_order_id,
+        externalReference: req.query.external_reference,
+        paymentType: req.query.payment_type
+	})
+})
+
+app.get('/pending', (req, res) => {
+	res.render('pending',{
+		payment: req.query.payment_id,
+		status: req.query.status,
+		merchantOrder: req.query.merchant_order_id,
+        externalReference: req.query.external_reference,
+        paymentType: req.query.payment_type
+	})
+})
+
+app.post('/webhook', async (req, res) => {
+    const payment = req.query
+    /*console.log(payment)
+    if(payment.type === 'payment'){
+        const data = await preference.search(payment['data.id'])
+        console.log(data)
+    }*/
+    
+    res.json({result: payment})
+})
+
+
+app.listen(port, () => {
+    console.log(`Server on port ${port}`)
+})
---
+++ assets/index.js
@@ -0,0 +1,54 @@
+const mp = new MercadoPago('APP_USR-ff96fe80-6866-4888-847e-c69250754d38');
+
+
+document.getElementById('checkout-btn').addEventListener('click', () => {
+    const orderData = {  
+        id: 1234,
+        img: document.getElementById('img').currentSrc,
+        title: document.getElementById('title').innerHTML,
+        unit: document.getElementById('unit').innerHTML,
+        price: document.getElementById('price').innerHTML
+    }
+    console.log(orderData)
+    fetch('https://magalizap-mp-commerce-nodejs.onrender.com/detail', { 
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(orderData)
+        
+    })
+    .then((response) => {
+        return response.json()
+    })
+    .then((preference) => {
+        createCheckoutButton(preference.id)
+    })
+    .catch((error) => console.log(error))
+})
+
+const createCheckoutButton = (preferenceId) => {
+    const bricksBuilder = mp.bricks()
+    const renderComponent = async (bricksBuilder) => {
+        if(window.checkoutButton) window.checkoutButton.unmount()
+        await bricksBuilder.create('wallet', 'button-checkout', {
+            initialization: {
+                preferenceId: preferenceId,
+                redirectMode: 'modal'
+            },
+            customization: {
+                visual: {
+                    buttonBackground: 'black',
+                },
+                checkout: {
+                    theme: {
+                        elementsColor: "#4287F5",
+                        headerColor: "#4287F5",
+                    },
+                },
+            },
+        })
+    }
+    window.checkoutButton = renderComponent(bricksBuilder)
+}
+
